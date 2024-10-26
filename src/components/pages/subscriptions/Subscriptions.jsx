0773b387c2a3f957bac52f1050d7a0bb6fa0f445
Emre <ahmet.emre.cakmak@yandex.com>
2024-10-26 18:56:21 +0300

    subscriptions added

--- src/components/pages/subscriptions/Subscriptions.jsx
+++ src/components/pages/subscriptions/Subscriptions.jsx
@@ -4,6 +4,17 @@ const Subscriptions = () => {
   return (
     <div className="subscriptions">
       <h1>Subscriptions</h1>
+      <div className="plans">
+        <div className="plan1">
+          <h3>AI-Strolog Free</h3>
+        </div>
+        <div className="plan2">
+          <h3>AI-Strolog Plus</h3>
+        </div>
+        <div className="plan3">
+          <h3>AI-Strolog Premium</h3>
+        </div>
+      </div>
     </div>
   )
 }
